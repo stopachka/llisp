0921823ab839a7c7245380e56e9f0af65cec45b3
stopachka <stepan.p@gmail.com>
2022-01-14 10:38:59 -0500

    play around with let

--- src/simple_lisp/core.clj
+++ src/simple_lisp/core.clj
@@ -17,13 +17,12 @@
 (def macro? (partial seq-starts-with? 'mac))
 
 (def literal?
-  (some-fn string? number? boolean? char? closure? macro? fn? nil?))
+  (some-fn string? number? boolean? char? nil? fn? closure? macro?))
 
 (declare eval eval-many)
 
 (defn lookup-symbol [{:keys [globe scope]} sym]
-  (let [v (some (fn [m] (when (contains? m sym)
-                          [(get m sym)]))
+  (let [v (some (fn [m] (when (contains? m sym) [(get m sym)]))
                 [{'scope scope} globe scope])]
     (assert v (format "expected value for sym = %s" sym))
     (first v)))
@@ -33,29 +32,25 @@
   (.put (:globe env) k (eval env v)))
 
 (defn eval-if [env [_ test-form when-true when-false]]
-  (let [evaled-v (eval env test-form)]
-    (eval env (if evaled-v when-true when-false))))
+  (let [evaled-test (eval env test-form)]
+    (eval env (if evaled-test when-true when-false))))
 
 (defn assign-vars [scope syms args]
   (merge scope (into {} (map vector syms args))))
 
-(defn eval-closure [env clo args]
-  (let [[_ scope syms body] clo
-        _ (assert (= (count syms) (count args))
-                  (format "syms and args must match syms: %s args: %s"
-                          (vec syms) (vec args)))
-        new-scope (assign-vars scope syms args)]
-    (eval (assoc env :scope new-scope) body)))
+(defn eval-closure [env [_ scope syms body] args]
+  (assert (= (count syms) (count args))
+          (format "syms and args must match syms: %s args: %s"
+                  (vec syms) (vec args)))
+  (eval (assoc env :scope (assign-vars scope syms args)) body))
 
-(defn eval-macro [env mac args]
-  (let [[_ clo] mac]
-    (eval env
-          (eval env
-                (concat [clo] (map (fn [x] (list 'quote x)) args))))))
+(defn eval-macro [env [_ clo] args]
+  (eval env
+        (eval env
+              (concat [clo] (map (fn [x] (list 'quote x)) args)))))
 
-(defn eval-application [env form]
-  (let [[f & args] form
-        f-evaled (eval env f)]
+(defn eval-application [env [f & args]]
+  (let [f-evaled (eval env f)]
     (cond
       (fn? f-evaled) (apply f-evaled (eval-many env args))
       (closure? f-evaled) (eval-closure env f-evaled (eval-many env args))
@@ -83,13 +78,14 @@
       (println "> ")
       (println (eval e (read)))
       (recur))))
+
 (comment
   (eval (env) "foo")
   (eval (env) 1.2)
   (eval (env) '(clo nil (x) (+ 1 x)))
   (let [e (env)]
     (eval e '(def foo 'bar))
-    e)
+    (eval e 'foo))
   (eval (env) '(if true 'foo 'bar))
   (eval (env) '(+ 1 2))
   (eval (env) '((clo nil (x) (+ x 1)) 4))
@@ -105,5 +101,12 @@
                    (list 'list ''clo 'scope
                          (list 'quote args)
                          (list 'quote body)))
-                '(((fn (x) (fn (y) (+ x y))) 1) 2)])))
+                '(defmacro let (pairs body)
+                   (concat
+                    [(list 'fn
+                          (map first pairs)
+                          body)]
+                    (map second pairs)))
+                '(((fn (x) (fn (y) (+ x y))) 1) 2)
+                '(let ((x 1) (y 2)) (+ x y))])))
 
